a46539dd8e0d0cac4d84e5d7951a0aa491e69890
Mihaly Bardos <bardosmisi@windowslive.com>
2020-03-08 11:07:47 +0100

    start to hide things

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Dec 02, 2017 15:37:25").getTime();
+		var countDownDate = new Date("Oct 10, 2020 16:30:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
@@ -274,15 +274,15 @@
 		// + minutes + "Minutes " + seconds + "Seconds ";
 
 		// Display the result in an element with id="demo"
-		document.getElementById("days").innerHTML = days +" <small>days</small>";
-		document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
-		document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-		document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+		document.getElementById("days").innerHTML = days +" <small>nap</small>";
+		document.getElementById("hours").innerHTML = hours + " <small>óra</small> ";
+		document.getElementById("minutes").innerHTML = minutes + " <small>perc</small> ";
+		document.getElementById("seconds").innerHTML = seconds + " <small>másodperc</small> ";
 
 		// If the count down is finished, write some text 
 		if (distance < 0) {
 		 clearInterval(x);
-		 document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
+		 //document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
 		}
 		}, 1000);	
 	
